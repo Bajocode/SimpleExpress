9dd36250926e9dc2756557dcc99798bddedd9208
Fabijan Bajo <fabijan.bajo-cic.netherlands@ibm.com>
2018-05-27 00:07:12 +0200

    Finish base app and bin

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const httpError = require('http-errors');
+const IndexRouter = require('./routes/IndexRouter');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', IndexRouter);
+
+app.use((req, res, next) => {
+  next(httpError(404));
+});
+
+app.use((err, req, res) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
 
