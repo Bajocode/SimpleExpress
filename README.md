622aa8e0b14ce5906f3af706b37795cf36b12a54
Fabijan Bajo <fabijan.bajo-cic.netherlands@ibm.com>
2018-06-05 07:59:43 +0200

    Add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple Express
+
+A basic webserver and front-end, letting users signup and login, built with `express`, `mongodb` and `ejs`.
---
+++ controllers/authController.js
@@ -0,0 +1,34 @@
+const User = require('../models/User');
+const passport = require('passport');
+
+exports.getLogin = (req, res) => res.render('login');
+
+exports.postLogin = (passport.authenticate('login', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true,
+}));
+
+exports.getLogout = (req, res) => {
+  req.logout();
+  return res.redirect('/');
+};
+
+exports.getSignup = (req, res) => res.render('signup');
+
+exports.postSignup = ((req, res, next) => {
+  const { username } = req.body;
+  const { password } = req.body;
+
+  User.findOne({ username }, (err, user) => {
+    if (err) return next(err);
+    if (user) {
+      req.flash('error', 'User already exists');
+      return res.redirect('/signup');
+    }
+
+    const newUser = new User({ username, password });
+
+    return newUser.save(next);
+  });
+});
