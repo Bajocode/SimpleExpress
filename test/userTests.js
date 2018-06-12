34e0e4105eb8535e9ac309650f7efb348e96f710
Fabijan Bajo <fabijan.bajo-cic.netherlands@ibm.com>
2018-06-12 20:19:56 +0200

    Model tests

--- test/userTests.js
+++ test/userTests.js
@@ -1,14 +1,59 @@
-const expect = require('chai').expect;
+const { expect } = require('chai');
+const sinon = require('sinon');
+const bcrypt = require('bcrypt-nodejs');
 
 const sut = require('../models/User');
 
-describe('sut', () => {
-  it('should require a username', (done) => {
-    const user = new sut()
+describe('User', () => {
 
-    user.validate((err) => {
+  let emptyUser;
+  let user;
+
+  beforeEach(() => {
+    emptyUser = new sut();
+    user = new sut({
+      username: 'foo',
+      password: 'bar',
+      displayName: 'baz',
+      bio: 'qux',
+    });
+  });
+
+  afterEach(() => {
+    emptyUser = null;
+    user = null;
+  })
+
+  it('should require a username', (done) => {
+    emptyUser.validate((err) => {
       expect(err.errors.username).to.exist;
       done();
     });
+  });
+
+  it('should require a password', (done) => {
+    emptyUser.validate((err) => {
+      expect(err.errors.password).to.exist;
+      done();
+    });
+  });
+
+  it('should set all properties correctly', (done) => {
+    expect(user.username).to.equal('foo');
+    expect(user.password).to.equal('bar');
+    expect(user.displayName).to.equal('baz');
+    expect(user.bio).to.equal('qux');
+    done();
+  });
+
+  it('should return a username when no displayName set', (done) => {
+    emptyUser.username = 'foo';
+    expect(emptyUser.getName()).to.equal('foo');
+    done();
+  });
+
+  it('should return a displayname when displayName set', (done) => {
+    expect(user.getName()).to.equal('baz');
+    done();
   });
 });
